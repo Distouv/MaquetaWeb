925a6a80709b4372fee605e7310ee2b726d883a5
Ray <raimundo.iligaray@inacapmail.cl>
2024-06-09 19:16:55 -0400

    Update scrollToSection function to use smooth scrolling

--- res/scripts/main.js
+++ res/scripts/main.js
@@ -3,8 +3,8 @@ function scrollToSection(selector, duration, offset) {
     if (element) {
       const topPos = element.getBoundingClientRect().top + window.pageYOffset + (offset || 0);
       window.scrollTo({
-        top: topPos, // Posición en Y del elemento ajustada por el offset
-        behavior: 'smooth' // Efecto de desplazamiento suave
+        top: topPos, 
+        behavior: 'smooth'
       });
     }
   }
