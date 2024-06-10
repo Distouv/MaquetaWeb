a6531967a5ee32722d13b0255f712bf2b9f53ec4
Ray <raimundo.iligaray@inacapmail.cl>
2024-06-09 20:20:08 -0400

    Update scrollToSection function to use smooth scrolling and update navigation links to scroll to specific sections on click

--- res/scripts/main.js
+++ res/scripts/main.js
@@ -7,4 +7,35 @@ function scrollToSection(selector, duration, offset) {
         behavior: 'smooth'
       });
     }
-  }
+  };
+
+
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav li a');
+  
+    function updateNavLinks() {
+      let currentSection = '';
+      sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= (sectionTop - sectionHeight / 3) && pageYOffset < (sectionTop + sectionHeight - sectionHeight / 3)) {
+          currentSection = section.getAttribute('id');
+        }
+      });
+  
+      navLinks.forEach(link => {
+        // Quita active de todos los enlaces
+        link.classList.remove('active');
+        const sectionId = link.getAttribute('onclick').match(/'#(.*?)'/)[1];
+
+        if (sectionId === currentSection) {
+          link.classList.add('active');
+        }
+      });
+    }
+  
+    window.addEventListener('scroll', updateNavLinks);
+    updateNavLinks(); // Llama a la función inicialmente para establecer el estado activo correcto al cargar la página
+  });
